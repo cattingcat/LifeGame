1c95576a242827698e1bfcfd2ef5ca337712eebe
zxc123zxc <mark.martynov@yahoo.com>
2014-01-28 01:36:32 +0400

    sdf

--- graphicswidget.h
+++ graphicswidget.h
@@ -5,6 +5,7 @@
 #include <QPainter>
 #include <QWidget>
 #include <vector>
+#include <list>
 #include <iostream>
 #include <QPixmap>
 
@@ -12,34 +13,40 @@
 class GraphicsWidget: public QWidget{
     Q_OBJECT
 private:
-    std::vector<std::vector<bool>> _field;
+    std::vector<std::vector<bool>> field;
+    std::list<std::vector<std::vector<bool>>> log;
+    uint column_count;
+    uint row_count;
+    uint max_log_length;
 
 public:    
     GraphicsWidget(QWidget* parent = 0, Qt::WindowFlags f = 0): QWidget(parent, f){
+        column_count = 50;
+        row_count = 50;
+        max_log_length = 70;
+        field = createField();
+        setField(field);
     }
 
     ~GraphicsWidget(){
     }
 
 private:
-    int calcCellSize(int col_count, int row_count){
+    int calcCellSize(){
         int w = this->width();
         int h = this->height();
         int min_dimension = w > h ? h : w;
-        min_dimension -= min_dimension % col_count;
-        int cell_size = min_dimension / col_count;
+        min_dimension -= min_dimension % column_count;
+        int cell_size = min_dimension / column_count;
         return cell_size;
     }
 
     void paintEvent(QPaintEvent*){
         QPainter p(this);
-
-        if(!_field.empty()){
-            int col_count = _field.size();
-            int row_count = _field.begin()->size();
-
-            int cell_size = calcCellSize(col_count, row_count);
-
+        if(!field.empty()){
+            int col_count = field.size();
+            int row_count = field.begin()->size();
+            int cell_size = calcCellSize();
             int next_vert = 0;
             int next_hor = 0;
             for(int i = 0; i < col_count; ++i, next_vert += cell_size){
@@ -47,7 +54,7 @@ private:
                 next_hor = 0;
                 for(int j = 0; j < row_count; ++j, next_hor += cell_size){
                     p.drawLine(0, next_hor, row_count * cell_size, next_hor);
-                    if(_field[i][j])
+                    if(field[i][j])
                         p.fillRect(next_vert, next_hor, cell_size, cell_size, Qt::SolidPattern);
                 }
             }
@@ -57,27 +64,76 @@ private:
     }
 
     void mousePressEvent(QMouseEvent *e){
-        int col_count = _field.size();
-        int row_count = _field.begin()->size();
-        int cell_size = calcCellSize(col_count, row_count);
-        if(e->x() < col_count * cell_size && e->y() < row_count * cell_size){
+        int cell_size = calcCellSize();
+        if(e->x() < column_count * cell_size && e->y() < row_count * cell_size){
             int column = e->x() / cell_size;
             int row = e->y() / cell_size;
-            bool b = _field[column][row];
-            _field[column][row] = !b;
+            bool b = field[column][row];
+            field[column][row] = !b;
             repaint();
         }
     }
 
-public:
+    std::vector<std::vector<bool>> createField(){
+        std::vector<std::vector<bool>> m = std::vector<std::vector<bool>>(column_count);
+        for(uint i = 0; i < column_count; ++i)
+            m[i] = std::vector<bool>(row_count);
+        return m;
+    }
+
+    void calculateNext(){
+        std::vector<std::vector<bool>> last_field = *log.rbegin();
+        for(uint x = 1; x < last_field.size() - 1; ++x){
+            for(uint y = 1; y < (*last_field.begin()).size() - 1; ++y){
+                field[x][y] = false;
+                int nc = calculateNeighbor(x, y);
+                if((!last_field[x][y]) && nc == 3)
+                    field[x][y] = true;
+                if((last_field[x][y])){
+                    if(nc == 3 || nc == 2)
+                        field[x][y] = true;
+                    else
+                        field[x][y] = false;
+                }
+            }
+        }
+    }
+
+    int calculateNeighbor(int x, int y){
+        std::vector<std::vector<bool>> last_field = *log.rbegin();
+        int res = 0;
+        for(int i = x - 1; i <= x + 1; ++i)
+            for(int j = y - 1; j <= y + 1; ++j)
+                if(last_field[i][j])
+                    ++res;
+        if(last_field[x][y])
+            --res;
+        return res;
+    }
+
+
+public slots:
     void setField(std::vector<std::vector<bool>> m){
-        _field = m;
+        field = m;
         repaint();
     }
 
     std::vector<std::vector<bool>> getField(){
-        return _field;
+        return field;
     }
+
+    void next(){
+        if(log.size() < max_log_length){
+            log.push_back(field);
+        } else {
+            log.pop_front();
+            log.push_back(field);
+        }
+        field = createField();
+        calculateNext();
+        setField(field);
+    }
+
 };
 
 #endif
--- mainwindow.h
+++ mainwindow.h
@@ -14,101 +14,21 @@ private:
     GraphicsWidget *gw;
     QTimer *timer;
     Ui::MainWindow *ui;
-    std::vector<std::vector<bool>> field;
-
-    std::list<std::vector<std::vector<bool>>> log;
-
-    uint column_count;
-    uint row_count;
-
-    uint max_log_length;
 
 public:
     MainWindow(QWidget *parent = 0): QMainWindow(parent), ui(new Ui::MainWindow){
-        column_count = 50;
-        row_count = 50;
-        max_log_length = 70;
-
         ui->setupUi(this);
         gw = new GraphicsWidget;
         ui->verticalLayout->addWidget(gw);
-
         timer = new QTimer(this);
         timer->setInterval(100);
-        connect(timer, SIGNAL(timeout()), this, SLOT(timerSlot()));
-
+        connect(timer, SIGNAL(timeout()), gw, SLOT(next()));
         connect(ui->startBtn, SIGNAL(clicked()), timer, SLOT(start()));
         connect(ui->finishBtn, SIGNAL(clicked()), timer, SLOT(stop()));
-
-        field = createField();
-
-        gw->setField(field);
-  }
+    }
 
     ~MainWindow(){
         delete ui;
-    }
-
-private:
-    std::vector<std::vector<bool>> createField(){
-        std::vector<std::vector<bool>> m = std::vector<std::vector<bool>>(column_count);
-        for(int i = 0; i < column_count; ++i)
-            m[i] = std::vector<bool>(row_count);
-        return m;
-    }
-
-    void calculateNext(){
-        std::vector<std::vector<bool>> last_field = *log.rbegin();
-        for(uint x = 1; x < last_field.size() - 1; ++x){
-            for(uint y = 1; y < (*last_field.begin()).size() - 1; ++y){
-                field[x][y] = false;
-                int nc = calcHeigb(x, y);
-                if((!last_field[x][y]) && nc == 3)
-                    field[x][y] = true;
-                if((last_field[x][y])){
-                    if(nc == 3 || nc == 2)
-                        field[x][y] = true;
-                    else
-                        field[x][y] = false;
-                }
-            }
-        }
-    }
-
-    int calcHeigb(int x, int y){
-        std::vector<std::vector<bool>> last_field = *log.rbegin();
-        int res = 0;
-        for(int i = x - 1; i <= x + 1; ++i)
-            for(int j = y - 1; j <= y + 1; ++j)
-                if(last_field[i][j])
-                    ++res;
-        if(last_field[x][y])
-            --res;
-        return res;
-    }
-
-    void print_field(){
-        for(uint x = 1; x < field.size() - 1; ++x){
-            for(uint y = 1; y < (*field.begin()).size() - 1; ++y){
-                std::cout<<field[x][y];
-            }
-            std::cout<<std::endl;
-        }
-        std::cout<<std::endl;
-    }
-    
-
-private slots:
-    void timerSlot(){
-        if(log.size() < max_log_length){
-            log.push_back(gw->getField());
-        } else {
-            log.pop_front();
-            log.push_back(gw->getField());
-        }
-        field = createField();
-        calculateNext();
-        gw->setField(field);
     }
 };
 
